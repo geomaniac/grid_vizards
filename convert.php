54911167503c2d4f5ab4deb7146ce85e0eaa4ed9
Fabian Kampe <fabian.kampe@giga-infosystems.com>
2018-06-09 13:44:04 +0200

    richtiges convert script

--- convert.php
+++ convert.php
@@ -1,8 +1,14 @@
 <?php
-$fileName = "voxet_newtest_voxet_gold@@";
+$fileName = "voxet_newtest_voxet_zinc@@";
         if (file_exists($fileName)) {
             $hex = file_get_contents($fileName);
-            $dec = unpack("g*", $hex);
-            echo $dec[0];
+            $dec = unpack("G*", $hex);
+            $hex = "";
+            for ($i=1;$i<=count($dec);$i++) {
+                $hex .= pack("g", $dec[$i]);
+
+            }
+            //var_dump($hex);
+            file_put_contents($fileName.'_le', $hex);
         }
 ?>
