c8d0234f72fe6109a2c602bab499c9106c60eb5b
Daniel Buse <daniel.buse@giga-infosystems.com>
2018-06-10 11:51:50 +0200

    fix formatting

--- three.js/js/single_cubes.js
+++ three.js/js/single_cubes.js
@@ -10,7 +10,7 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         this.draw_y_slice(this.grid.cell_count.y - 1);
         this.draw_z_slice(this.grid.cell_count.z - 1);
     }
-    this.init = function() {
+    this.init = function () {
         this.draw_all_cells();
     }
     this.add_cube = function (pos, color) {
@@ -52,7 +52,7 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (y = 0; y < this.grid.cell_count.y; ++y) {
                 for (x = 0; x < this.grid.cell_count.x; ++x) {
-                    if(this.is_nodata_val(0, x, y, z)) {
+                    if (this.is_nodata_val(0, x, y, z)) {
                         continue;
                     }
                     ++count;
@@ -82,19 +82,19 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var x = num;
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (y = 0; y < this.grid.cell_count.y; ++y) {
-                if(this.is_nodata_val(0, x, y, z)) {
+                if (this.is_nodata_val(0, x, y, z)) {
                     continue;
                 }
-				vec = new THREE.Vector3(0, 0, 0);
-				vec.addScaledVector(this.grid.axis.w, z);
-				vec.addScaledVector(this.grid.axis.v, y);
-				vec.addScaledVector(this.grid.axis.u, x);
-				positions.push(vec.x, vec.y, vec.z);
-				color = new THREE.Color(this.get_color(0, x, y, z));
-				colors.push(color.r, color.g, color.b);
+                vec = new THREE.Vector3(0, 0, 0);
+                vec.addScaledVector(this.grid.axis.w, z);
+                vec.addScaledVector(this.grid.axis.v, y);
+                vec.addScaledVector(this.grid.axis.u, x);
+                positions.push(vec.x, vec.y, vec.z);
+                color = new THREE.Color(this.get_color(0, x, y, z));
+                colors.push(color.r, color.g, color.b);
             }
         }
-		geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
         geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
         geometry.computeBoundingSphere();
         var material = new THREE.PointsMaterial({ size: 1, vertexColors: THREE.VertexColors });
@@ -109,19 +109,19 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var y = num;
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (x = 0; x < this.grid.cell_count.x; ++x) {
-                if(this.is_nodata_val(0, x, y, z)) {
+                if (this.is_nodata_val(0, x, y, z)) {
                     continue;
                 }
-				vec = new THREE.Vector3(0, 0, 0);
-				vec.addScaledVector(this.grid.axis.w, z);
-				vec.addScaledVector(this.grid.axis.v, y);
-				vec.addScaledVector(this.grid.axis.u, x);
-				positions.push(vec.x, vec.y, vec.z);
-				color = new THREE.Color(this.get_color(0, x, y, z));
-				colors.push(color.r, color.g, color.b);
+                vec = new THREE.Vector3(0, 0, 0);
+                vec.addScaledVector(this.grid.axis.w, z);
+                vec.addScaledVector(this.grid.axis.v, y);
+                vec.addScaledVector(this.grid.axis.u, x);
+                positions.push(vec.x, vec.y, vec.z);
+                color = new THREE.Color(this.get_color(0, x, y, z));
+                colors.push(color.r, color.g, color.b);
             }
         }
-		geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
         geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
         geometry.computeBoundingSphere();
         var material = new THREE.PointsMaterial({ size: 1, vertexColors: THREE.VertexColors });
@@ -136,19 +136,19 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var z = num;
         for (y = 0; y < this.grid.cell_count.y; ++y) {
             for (x = 0; x < this.grid.cell_count.x; ++x) {
-                if(this.is_nodata_val(0, x, y, z)) {
+                if (this.is_nodata_val(0, x, y, z)) {
                     continue;
                 }
-				vec = new THREE.Vector3(0, 0, 0);
-				vec.addScaledVector(this.grid.axis.w, z);
-				vec.addScaledVector(this.grid.axis.v, y);
-				vec.addScaledVector(this.grid.axis.u, x);
-				positions.push(vec.x, vec.y, vec.z);
-				color = new THREE.Color(this.get_color(0, x, y, z));
-				colors.push(color.r, color.g, color.b);
+                vec = new THREE.Vector3(0, 0, 0);
+                vec.addScaledVector(this.grid.axis.w, z);
+                vec.addScaledVector(this.grid.axis.v, y);
+                vec.addScaledVector(this.grid.axis.u, x);
+                positions.push(vec.x, vec.y, vec.z);
+                color = new THREE.Color(this.get_color(0, x, y, z));
+                colors.push(color.r, color.g, color.b);
             }
         }
-		geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
         geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
         geometry.computeBoundingSphere();
         var material = new THREE.PointsMaterial({ size: 1, vertexColors: THREE.VertexColors });
