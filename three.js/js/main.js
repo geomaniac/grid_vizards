001aaa70062853008161055ee3d1eecedc8ae2d6
Daniel Buse <daniel.buse@giga-infosystems.com>
2018-06-10 13:41:41 +0200

    last stuff

--- three.js/js/main.js
+++ three.js/js/main.js
@@ -1,32 +1,32 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 100, 1000000);
-var controls = new THREE.OrbitControls(camera);
-var stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-var renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-var container = document.createElement('div');
-document.body.appendChild(container);
-container.appendChild(renderer.domElement);
-container.appendChild(stats.dom);
-//window.addEventListener('resize', onWindowResize, false);
-var o = new THREE.Vector3(0, 0, 0);
-var u = new THREE.Vector3(25, 0, 0);
-var v = new THREE.Vector3(0, 25, 0);
-var w = new THREE.Vector3(0, 0, 20);
-var cell_count = new THREE.Vector3(608, 682, 261);
-//var data = GRID_VIZARDS.random_array(1000000);
+var grid, renderer, scene, camera, stats, single_cubes, controls;
 var func = function (data) {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 100, 1000000);
+    controls = new THREE.OrbitControls(camera);
+    stats = new Stats();
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    var container = document.createElement('div');
+    document.body.appendChild(container);
+    container.appendChild(renderer.domElement);
+    container.appendChild(stats.dom);
+    //window.addEventListener('resize', onWindowResize, false);
+    var o = new THREE.Vector3(0, 0, 0);
+    var u = new THREE.Vector3(25, 0, 0);
+    var v = new THREE.Vector3(0, 25, 0);
+    var w = new THREE.Vector3(0, 0, 20);
+    var cell_count = new THREE.Vector3(608, 682, 261);
+    //var data = GRID_VIZARDS.random_array(1000000);
     var props = [new GRID_VIZARDS.PROP("", data)];
-    var grid = new GRID_VIZARDS.GRID(o, u, v, w, cell_count, props);
-    var single_cubes = new GRID_VIZARDS.SingleCubes(scene, grid);
+    grid = new GRID_VIZARDS.GRID(o, u, v, w, cell_count, props);
+    single_cubes = new GRID_VIZARDS.SingleCubes(scene, grid);
     single_cubes.init();
+    camera.position.z = 500;
     animate();
 };
-var data = GRID_VIZARDS.data_array(func);
-
-camera.position.z = 5;
+GRID_VIZARDS.data_array(func);
 
 function animate() {
     requestAnimationFrame(animate);
