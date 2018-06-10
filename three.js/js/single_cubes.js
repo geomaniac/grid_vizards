20ab4101fc9084c64d7916bff0d0a63c51127d7d
Daniel Buse <daniel.buse@giga-infosystems.com>
2018-06-10 11:30:28 +0200

    get real data in there.
    NOTE: now needs a local webserver running on root grid_vizards

--- three.js/js/single_cubes.js
+++ three.js/js/single_cubes.js
@@ -2,7 +2,7 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
     this.scene = scene;
     this.grid = grid;
     this.geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
-    this.init = function () {
+    this.init_ = function () {
         this.draw_x_slice(0);
         this.draw_y_slice(0);
         this.draw_z_slice(0);
@@ -10,7 +10,7 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         this.draw_y_slice(this.grid.cell_count.y - 1);
         this.draw_z_slice(this.grid.cell_count.z - 1);
     }
-    this.init_ = function() {
+    this.init = function() {
         this.draw_all_cells();
     }
     this.add_cube = function (pos, color) {
@@ -25,22 +25,38 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var prop = this.grid.props[prop_index];
         var data_index = this.grid.get_array_index(u, v, w);
         var value = prop.data[data_index];
-        if (value < 34) {
+        if (value < -6.75) {
+            return 0x0000ff;
+        } else if (value < -5) {
+            return 0x00ffff;
+        } else if (value < -3.75) {
             return 0x00ff00;
-        } else if (value < 67) {
+        } else if (value < -2.5) {
             return 0xffff00;
         } else {
             return 0xff0000;
         }
+    }
+    this.is_nodata_val = function (prop_index, u, v, w) {
+        var prop = this.grid.props[prop_index];
+        var data_index = this.grid.get_array_index(u, v, w);
+        var value = prop.data[data_index];
+        return value === -99999.;
     }
     this.draw_all_cells = function () {
         var vec, color;
         var geometry = new THREE.BufferGeometry();
         var positions = [];
         var colors = [];
+        let count = 0;
+        console.log(this.grid.props[0].data[0]);
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (y = 0; y < this.grid.cell_count.y; ++y) {
                 for (x = 0; x < this.grid.cell_count.x; ++x) {
+                    if(this.is_nodata_val(0, x, y, z)) {
+                        continue;
+                    }
+                    ++count;
                     vec = new THREE.Vector3(0, 0, 0);
                     vec.addScaledVector(this.grid.axis.w, z);
                     vec.addScaledVector(this.grid.axis.v, y);
@@ -51,11 +67,12 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
                 }
             }
         }
+        console.log(count);
         geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
         geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
         geometry.computeBoundingSphere();
         geometry.center();
-        var material = new THREE.PointsMaterial({ size: 1, vertexColors: THREE.VertexColors });
+        var material = new THREE.PointsMaterial({ size: 25, vertexColors: THREE.VertexColors });
         points = new THREE.Points(geometry, material);
         scene.add(points);
     }
@@ -67,6 +84,9 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var x = num;
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (y = 0; y < this.grid.cell_count.y; ++y) {
+                if(this.is_nodata_val(0, x, y, z)) {
+                    continue;
+                }
 				vec = new THREE.Vector3(0, 0, 0);
 				vec.addScaledVector(this.grid.axis.w, z);
 				vec.addScaledVector(this.grid.axis.v, y);
@@ -91,6 +111,9 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var y = num;
         for (z = 0; z < this.grid.cell_count.z; ++z) {
             for (x = 0; x < this.grid.cell_count.x; ++x) {
+                if(this.is_nodata_val(0, x, y, z)) {
+                    continue;
+                }
 				vec = new THREE.Vector3(0, 0, 0);
 				vec.addScaledVector(this.grid.axis.w, z);
 				vec.addScaledVector(this.grid.axis.v, y);
@@ -115,6 +138,9 @@ GRID_VIZARDS.SingleCubes = function (scene, grid) {
         var z = num;
         for (y = 0; y < this.grid.cell_count.y; ++y) {
             for (x = 0; x < this.grid.cell_count.x; ++x) {
+                if(this.is_nodata_val(0, x, y, z)) {
+                    continue;
+                }
 				vec = new THREE.Vector3(0, 0, 0);
 				vec.addScaledVector(this.grid.axis.w, z);
 				vec.addScaledVector(this.grid.axis.v, y);
--- three.js/js/utils.js
+++ three.js/js/utils.js
@@ -5,3 +5,32 @@ GRID_VIZARDS.random_array = function (num_elements) {
     }
     return arr;
 }
+
+GRID_VIZARDS.data_array = function (func) {
+    var req = new XMLHttpRequest();
+    //req.open("GET", "/three.js/data/hackathon_grid_property_geol_modeled_multi_se_20170406@@_le", true);
+    req.open("GET", "/three.js/data/hackathon_grid_property_modeled_logk@@_le", true);
+    req.responseType = "arraybuffer";
+
+    var data = [];
+
+    req.onload = function () {
+        var arrayBuffer = req.response;
+        if (arrayBuffer) {
+            console.log(arrayBuffer)
+            var byteArray = new Float32Array(arrayBuffer);
+            console.log(byteArray.length)
+            data = Array.prototype.slice.call(byteArray);
+            var min = 999999999999;
+            var max = -999999999999;
+            for(var i = 0, l = data.length; i < l; ++i) {
+                if(data[i] === -99999) continue;
+                min = Math.min(min, data[i]);
+                max = Math.max(max, data[i]);
+            }
+            console.log(min);console.log(max);
+        }
+        func(data);
+    };
+    req.send(null);
+}
